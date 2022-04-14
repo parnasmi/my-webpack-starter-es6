742d08776db1831b923e1c3a18ac86eec577178a
Ilhom Maksadkulov <parnasmi@gmail.com>
2022-04-14 20:48:45 +0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './styles/style.css'
+import testImg from './assets/test-img.png'
+console.log("Hello World!2");
+const imgElem = document.getElementById('test-img');
+imgElem.src=testImg;
+
+
