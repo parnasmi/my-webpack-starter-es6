bb873752a830956e8abd2cabbe6de6e5d8d17b04
Ilhom Maksadkulov <parnasmi@gmail.com>
2022-04-16 23:46:48 +0500

    configure eslint husky lint-staged

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles/style.css";
 import testImg from "./assets/test-img.png";
-console.log("Hello World!2");
+
 const imgElem = document.getElementById("test-img");
 imgElem.src = testImg;
